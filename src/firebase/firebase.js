441af338527b504a7d0dbf84c543b5d2aae81f46
joannajjliu <joanna.liu3774@gmail.com>
2020-04-22 23:20:30 -0400

    firebase arrays and retrievals

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,36 +15,79 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
+database.ref('expenses')
+    .once('value')
+    .then((snapshot) => {
+        console.log(snapshot.val());
+    })
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 99987,
+//     createdAt: 324324324324234
+// });
 
+// database.ref('notes/-M5_-Y-TSeLnyEK9iTXp').remove();
+// database.ref('notes').push({
+//     title: "Course topics",
+//     body: "My topics go here"
+// });
 
-database.ref().set({
-    name: 'Mojojoojo Wannnng',
-    age: 5,
-    isSingle: true,
-    job: 'Software Dev',
-    location: {
-        city: 'Toronto',
-        country: 'Mexico'
-    }
-}).then(() => {
-    console.log('Data is saved');
-}).catch((error) => {
-    console.log('This failed: ', error);
-});
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log( 'Error with data fetching: ', e);
+// });
 
-database.ref('age').set(27);
-database.ref('location/city').set("New York");
+// setTimeout(() => {
+//     database.ref('age').set(16);
+// }, 3500);
+// setTimeout(() => {
+//     database.ref().off(onValueChange);//parameter passed to 'off' removes one specific subscription
+// }, 7000);
+// setTimeout(() => {
+//     database.ref('age').set(21);
+// }, 10500);
 
-database.ref('attributes').set(
-    {
-        height: 73,
-        weight: 50
-    }
-).then(() => {
-    console.log("attributes running");
-}).catch((error) => {
-    console.log("attributes error: ", error);
-});
+// occurs once: won't be notified of changes in db
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log("Error fetching data", e);
+//     });
+
+    // database.ref().set({
+//     name: 'Mojojoojo Wannnng',
+//     age: 5,
+//     isSingle: true,
+//     job: 'Software Dev',
+//     location: {
+//         city: 'Toronto',
+//         country: 'Mexico'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((error) => {
+//     console.log('This failed: ', error);
+// });
+
+// database.ref('age').set(27);
+// database.ref('location/city').set("New York");
+
+// database.ref('attributes').set(
+//     {
+//         height: 73,
+//         weight: 50
+//     }
+// ).then(() => {
+//     console.log("attributes running");
+// }).catch((error) => {
+//     console.log("attributes error: ", error);
+// });
 
 // database.ref('isSingle').remove()
 //     .then(() => {
@@ -55,10 +98,10 @@ database.ref('attributes').set(
 
 // database.ref("age").set(null);
 
-database.ref().update({
-    job: 'Manager',
-    isSingle: null,
-    'location/city': 'Beijing'
-});
+// database.ref().update({
+//     job: 'Manager',
+//     isSingle: null,
+//     'location/city': 'Beijing'
+// });
 
 
